2d3b9beb983c1a0465beff8ecd3b3841be5381e6
hiokky <hiokky.work@gmail.com>
2024-10-11 17:04:46 +0900

    コンポーネントのブラッシュアップ

--- site/src/components/SectionTitle.tsx
+++ site/src/components/SectionTitle.tsx
@@ -1,34 +1,40 @@
-import { ReactNode } from 'react'
+import { ReactNode } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+import { twMerge } from 'tailwind-merge';
+
+const sectionTitleStyles = cva(
+  // ベースのスタイル
+  'text-zinc-900 font-bold tracking-tight',
+  {
+    variants: {
+      variant: {
+        plain: '',
+        underLine: 'relative pb-4 w-fit before:absolute before:-bottom-1 before:left-0 before:w-20 before:h-1 before:bg-zinc-900 after:absolute after:-bottom-1 after:left-0 after:-z-10 after:w-full after:h-1 after:bg-gray-300',
+        sideLine: 'flex justify-center items-center text-center before:w-10 before:lg:w-20 before:h-1 before:bg-zinc-900 before:mr-7 after:w-10 after:lg:w-20 after:h-1 after:bg-zinc-900 after:ml-7',
+        speechBubble: 'flex justify-center items-center text-center relative before:w-1 before:h-10 before:bg-zinc-900 before:mr-8 before:rotate-[-35deg] after:w-1 after:h-10 after:bg-zinc-900 after:ml-8 after:rotate-[35deg]',
+        solid: 'inline-block px-4 py-2 bg-gray-300',
+      },
+      size: {
+        xl2: 'text-xl lg:text-2xl',
+        xl3: 'text-xl lg:text-3xl',
+      },
+    },
+    defaultVariants: {
+      variant: 'plain',
+      size: 'xl2',
+    },
+  }
+);
 
 type SectionTitleProps = {
-  children: ReactNode
-  variant?: 'plain' | 'underline' | 'solid'
-  className?: string
-}
+  children: ReactNode;
+  className?: string;
+} & VariantProps<typeof sectionTitleStyles>;
 
-const SectionTitle = ({ children, variant = 'plain', className }: SectionTitleProps) => {
-  const baseCn = 'text-zinc-900 text-2xl font-bold tracking-tight lg:text-3xl'
-  const underlineCn = 'pb-2 border-b-2 border-blue-900'
-  const solidCn = 'p-2 bg-blue-200 text-white'
+const SectionTitle = ({ children, variant, size, className }: SectionTitleProps) => {
+  return <h2 className={twMerge(sectionTitleStyles({ variant, size }), className)}>{children}</h2>;
+};
 
+SectionTitle.displayName = 'SectionTitle';
 
-  const selectCn = cn(
-    baseCn,
-    variant === 'plain' && '',
-    variant === 'underline' && underlineCn,
-    variant === 'solid' && solidCn,
-    className
-  )
-
-  return <h2 className={selectCn}>{children}</h2>
-}
-
-SectionTitle.displayName = 'SectionTitle'
-
-export { SectionTitle }
-
-
-//エラー回避のための暫定措置
-function cn(...classes: (string | undefined | null | false)[]): string {
-  return classes.filter(Boolean).join(' ')
-}
+export { SectionTitle };
