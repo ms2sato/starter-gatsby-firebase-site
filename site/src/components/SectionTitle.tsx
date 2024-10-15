334008155f5e8905f117b085960e956adbb4f7c4
hiokky <hiokky.work@gmail.com>
2024-10-15 16:56:03 +0900

    variantからshapeへ変更

--- site/src/components/SectionTitle.tsx
+++ site/src/components/SectionTitle.tsx
@@ -7,7 +7,7 @@ const sectionTitleStyles = cva(
   'text-zinc-900 font-bold text-xl tracking-tight',
   {
     variants: {
-      variant: {
+      shape: {
         plain: '',
         underLine: 'relative pb-4 w-fit before:absolute before:-bottom-1 before:left-0 before:w-20 before:h-1 before:bg-zinc-900 after:absolute after:-bottom-1 after:left-0 after:-z-10 after:w-full after:h-1 after:bg-gray-300',
         sideLine: 'flex justify-center items-center text-center before:w-10 before:lg:w-20 before:h-1 before:bg-zinc-900 before:mr-7 after:w-10 after:lg:w-20 after:h-1 after:bg-zinc-900 after:ml-7',
@@ -20,7 +20,7 @@ const sectionTitleStyles = cva(
       },
     },
     defaultVariants: {
-      variant: 'plain',
+      shape: 'plain',
       size: 'large',
     },
   }
@@ -31,8 +31,8 @@ type SectionTitleProps = {
   className?: string;
 } & VariantProps<typeof sectionTitleStyles>;
 
-const SectionTitle = ({ children, variant, size, className }: SectionTitleProps) => {
-  return <h2 className={twMerge(sectionTitleStyles({ variant, size }), className)}>{children}</h2>;
+const SectionTitle = ({ children, shape, size, className }: SectionTitleProps) => {
+  return <h2 className={twMerge(sectionTitleStyles({ shape, size }), className)}>{children}</h2>;
 };
 
 SectionTitle.displayName = 'SectionTitle';
