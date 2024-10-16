f5025b83c18cf44b80de154ef801f21067dfdc77
hiokky <hiokky.work@gmail.com>
2024-10-16 18:19:46 +0900

    eslint-plugin-tailwindcss導入

--- site/.eslintrc.js
+++ site/.eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
-  extends: `react-app`,
+  extends: [
+    `react-app`,
+    `plugin:tailwindcss/recommended`
+  ],
+  plugins: ['tailwindcss'],
 }
